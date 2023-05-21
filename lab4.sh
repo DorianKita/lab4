18de649e09a3f51ddb2d32812bc5819b0fec41a6
Dorian Kita <dorian.kita90@gmail.com>
2023-05-21 17:41:08 +0200

    added logs 30

--- lab4.sh
+++ lab4.sh
@@ -4,6 +4,7 @@ if [ "$1" == "--date" ]; then
     current_date=$(date)
     echo "Aktualna data: $current_date"
 fi
+
 if [ "$1" == "--logs" ]; then
     for ((i=1; i<=100; i++))
     do
@@ -15,4 +16,22 @@ if [ "$1" == "--logs" ]; then
     echo "Utworzono 100logów"
 else
     echo "nienzana flaga!"
+fi
+
+if [ "$1" == "--logs" ]; then
+    if [ -z "$2" ]; then
+        echo "Podaj liczbę plików do utworzenia."
+    else
+        logs=$2
+        for ((i=1; i<=logs; i++))
+        do
+            filename="log$i.txt"
+            echo "Nazwa pliku: $filename" > $filename
+            echo "Nazwa skryptu: $0" >> $filename
+            echo "Data utworzenia: $(date)" >> $filename
+        done
+        echo "Utworzono $logs plików log"
+    fi
+else
+    echo "Użyj --logs + liczba, aby utworzyć określoną liczbę logów."
 fi
