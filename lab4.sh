b6f3f79f105327b60e9f2b63f6fbda94596003c7
Dorian Kita <dorian.kita90@gmail.com>
2023-05-21 17:25:23 +0200

    added logs to script

--- lab4.sh
+++ lab4.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-    current_date=$(date)
-    echo "Aktualna data: $current_date"
+if [ "$1" == "--logs" ]; then
+    for ((i=1; i<=100; i++))
+    do
+        filename="log$i.txt"
+        echo "Nazwa pliku: $filename" > $filename
+        echo "Nazwa skryptu: $0" >> $filename
+        echo "Data utworzenia: $(date)" >> $filename
+    done
+    echo "Utworzono 100logów"
+else
+    echo "nienzana flaga!"
 fi
